b235341654fa085bc772fd6c87705a19f90a47e8
H4rsh99 <91564266+H4rsh99@users.noreply.github.com>
2023-09-13 01:24:23 +0530

    Update provider.tf

--- terraform_tool/provider.tf
+++ terraform_tool/provider.tf
@@ -1,13 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket = "ot-mohit-1"
-    dynamodb_table = "state-lock"
-    key = "terraform.tfstate"
-    region = "us-east-1"
-    encrypt = true
-    
-  }
-}
 
 provider "aws" {
   region = "us-east-1"
